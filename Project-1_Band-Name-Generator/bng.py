15d32b90d60e7716d74c39b6287d523e90541255
John King <lwhite4@sky.com>
2023-09-03 09:23:49 +0100

    first commit

---
+++ Project-1_Band-Name-Generator/bng.py
@@ -0,0 +1,8 @@
+# Project 1
+
+print("Welcome to the AWESOME band name generator 3000!!")
+
+city = input("What city did you grow up in?\n")
+pet = input("What was the name of your first pet?\n")
+
+print("Your band could be called " + city + " " + pet)
